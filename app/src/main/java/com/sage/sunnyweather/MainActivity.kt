915cd0391b4769f9f4f0ba65e09cf9df74f0d6c0
SageRover <1764575438@qq.com>
2022-04-13 16:46:13 +0800

    First commit.

---
+++ app/src/main/java/com/sage/sunnyweather/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.sage.sunnyweather
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+fatal: remote error:
+The unauthenticated git protocol on port 9418 is no longer supported.
+Please see https://github.blog/2021-09-01-improving-git-protocol-security-github/ for more information.
